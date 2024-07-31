109355a214e992e5b1124c78da4457ec2fcc4565
Karina Borges <karina.borges@druid.com.br>
2024-07-30 21:04:38 -0300

    ✨ Finish record component

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import "./styles/card.css";
 import "./styles/global.css";
 import "./styles/header.css";
 import "./styles/input.css";
+import "./styles/progress.css";
 import "./styles/record.css";
 import "./styles/user.css";
